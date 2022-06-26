9f49a3f0fd5a931eb9f082f73644f238047dba88
TimKapf <kapferer.tim@icloud.com>
2022-06-26 18:50:25 +0200

    Finalization of the Mayhem Map, commenting.

--- Assets/Scrips/Turret.cs
+++ Assets/Scrips/Turret.cs
@@ -12,17 +12,24 @@ public class Turret : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
+        // Get the GameObject car
         GameObject car = GameObject.FindWithTag("Car");
+        // Get the direction to the car
         Vector3 carDirection = car.transform.position - transform.position;
+        // Change the rotation of the current to always look at the car 
         transform.LookAt(car.transform);
+        // Use raycasts to shoot a sheel when its 10.0f away
         if (Physics.Raycast(transform.position, carDirection, out hit, 10.0f))
         {
+            // Always instantiate after a certain time
             if (frequency == 1.0f)
             {
+                // Create the shell object and release it towards the car
                 Instantiate(_shellPrefab, transform.position + transform.forward * 1.5f, transform.rotation);
         
             }
         }
+        // Reduce the the frequency variable until zero and than set it to 1 again
         frequency -= 0.02f;
         if (frequency < 0.0f)
         {
