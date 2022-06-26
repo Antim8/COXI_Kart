4653dd16cbf38f7b4c83dc4b4cc455c2a0a589e5
Antim8 <petersen.tim@outlook.de>
2022-06-26 04:35:37 +0200

    Main Menu working

---
+++ Assets/Scrips/MainMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string firstLevel;
+
+    public GameObject optionsScreen;
+    public GameObject levelSelection;
+    public bool OptionsScreenState {  get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        OptionsScreenState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        levelSelection.SetActive(true);
+        
+    }
+
+    public void HandleOptions() {
+
+        if (OptionsScreenState)
+        {
+            optionsScreen.SetActive(false);
+            OptionsScreenState = false;
+        }
+        else
+        {
+            optionsScreen.SetActive(true);
+            OptionsScreenState = true;
+        }
+        
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+}
---
+++ Assets/Scrips/OptionsScreen.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Toggle = UnityEngine.UI.Toggle;
+
+public class OptionsScreen : MonoBehaviour
+{
+
+    public Toggle fullScreenTog, vsyncTog;
+
+    public TMP_Dropdown resolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullScreenTog.isOn = Screen.fullScreen;
+        vsyncTog.isOn = QualitySettings.vSyncCount != 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGraphics()
+    {
+        var res = resolution.captionText.text;
+        var pxls = res.Split('x');
+        var x = Convert.ToInt32(pxls[0]);
+        var y = Convert.ToInt32(pxls[1]);
+        Screen.SetResolution(x, y, fullScreenTog.isOn);
+        Screen.fullScreen = fullScreenTog.isOn;
+        QualitySettings.vSyncCount = vsyncTog.isOn ? 1 : 0;
+    }
+}
